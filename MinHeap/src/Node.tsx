b8651703470a0f31cdb83330894075cc225f5803
neet-007 <moayedeffect@gmail.com>
2024-07-03 16:22:52 +0300

    the lines are placed in the rigth spot and update when the screen resizes but for some reason they are invisable and the ref of the left and right dont update as soon as they render

--- MinHeap/src/Node.tsx
+++ MinHeap/src/Node.tsx
@@ -1,4 +1,4 @@
-import {ComponentProps, forwardRef, useMemo, useRef} from 'react'
+import {ComponentProps, forwardRef, useEffect, useMemo, useRef, useState} from 'react'
 import { useTreeContext } from './TreeContext';
 
 export type NodeType = {
@@ -15,9 +15,24 @@ export interface NodeProps extends ComponentProps<'div'> {
 
 const Node = forwardRef<HTMLDivElement, NodeProps>(({ index, complete, ...props }, ref) => {
     const { heap, heapSize } = useTreeContext();
+    const [windowDimintions, setWindowDimintions] = useState<{width:number, height:number}>({width:0, height:0})
     const parentRef = useRef<HTMLParagraphElement>(null);
     const leftRef = useRef<HTMLDivElement>(null);
     const rightRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      function updateWindowSize(){
+        setWindowDimintions({
+          width:window.innerWidth,
+          height:window.innerHeight
+        });
+      };
+
+      window.addEventListener('resize', updateWindowSize);
+      updateWindowSize();
+
+      return() => window.removeEventListener('resize', updateWindowSize)
+    },[])
 
     const parentCords = useMemo(() => {
       if (parentRef.current) {
@@ -28,34 +43,44 @@ const Node = forwardRef<HTMLDivElement, NodeProps>(({ index, complete, ...props 
         };
       }
       return { x: 0, y: 0 };
-    }, [parentRef.current]);
+    }, [parentRef.current, windowDimintions.width, windowDimintions.height]);
 
     const leftCords = useMemo(() => {
         console.log(leftRef.current)
         if(!leftRef.current){
-            return {x:0, y:0};
+           return {x:0, y:0, height:0, width:0};
         };
         const rect = leftRef.current.children[0].getBoundingClientRect();
         console.log('left')
         console.log(rect)
-        return{
+        const val = {
             x:rect.x + rect.width / 2,
-            y:rect.y + rect.height /2
+            y:rect.y + rect.height / 2,
+            height:0,
+            width:0
         };
-    },[leftRef.current]);
+        val.height = Math.abs(val.y - parentCords.y);
+        val.width = Math.abs(val.x - parentCords.x);
+        return val
+    },[leftRef.current, windowDimintions.width, windowDimintions.height]);
 
     const rightCords = useMemo(() => {
         if(!rightRef.current){
-            return {x:0, y:0};
+            return {x:0, y:0, width:0, height:0};
         };
         const rect = rightRef.current.children[0].getBoundingClientRect();
         console.log('rigth')
         console.log(rect)
-        return{
+        const val = {
             x:rect.x + rect.width / 2,
-            y:rect.y + rect.height / 2
+            y:rect.y + rect.height / 2,
+            height:0,
+            width:0
         };
-    },[rightRef.current]);
+        val.height = Math.abs(val.y - parentCords.y);
+        val.width = Math.abs(val.x - parentCords.x);
+        return val
+    },[rightRef.current, windowDimintions.width, windowDimintions.height]);
 
     return (
       <div ref={ref} style={{width:'100%', display: 'flex', flexDirection: 'column' }} {...props}>
@@ -78,12 +103,12 @@ const Node = forwardRef<HTMLDivElement, NodeProps>(({ index, complete, ...props 
         </p>
         <div style={{display: 'flex', justifyContent: 'center' }}>
           {(heap[index].leftChild < heapSize && heap[index].leftChild !== -1) &&
-              <svg style={{ position: 'absolute' }} width="0" height="0">
+              <svg style={{ position: 'absolute', top:0, left:0, pointerEvents:'none' }} width={leftCords.width} height={leftCords.height}>
                 <line
-                x1={parentCords.x.toString()}
-                y1={parentCords.y.toString()}
-                x2={leftCords.x.toString()}
-                y2={rightCords.x.toString()}
+                x1={parentCords.x}
+                y1={parentCords.y}
+                x2={leftCords.x}
+                y2={leftCords.y}
                 stroke="black"
                 />
             </svg>
@@ -92,12 +117,12 @@ const Node = forwardRef<HTMLDivElement, NodeProps>(({ index, complete, ...props 
             <Node index={heap[index].leftChild} ref={leftRef} complete={heap[index].rightChild !== -1}/>
           }
           {(heap[index].rightChild < heapSize && heap[index].rightChild !== -1) &&
-              <svg style={{ position: 'absolute' }} width="0" height="0">
+              <svg style={{ position: 'absolute', top:0, left:0, pointerEvents:'none' }} width={rightCords.width} height={rightCords.height}>
                 <line
-                x1={parentCords.x.toString()}
-                y1={parentCords.y.toString()}
-                x2={rightCords.x.toString()}
-                y2={rightCords.y.toString()}
+                x1={parentCords.x}
+                y1={parentCords.y}
+                x2={rightCords.x}
+                y2={rightCords.y}
                 stroke="black"
                  />
                 </svg>
